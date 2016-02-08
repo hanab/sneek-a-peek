0a84ed151c1471d59ae065a5ae78486aa762d23d
Hana Demas <hb16826@gmail.com>
2016-02-08 17:38:29 +0000

    createRegisterationTable.php edited online with Bitbucket

--- createRegisterationTable.php
+++ createRegisterationTable.php
@@ -16,8 +16,8 @@ $sqlCommand = "CREATE TABLE register (
                  PRIMARY KEY (id),
 		 		 UNIQUE KEY username (username)
 		 		 ) ";
-if (mysqli_query($con->$con_db, $sqlCommand)){ 
-    //echo "Your registration table has been created successfully!"; 
+if (mysqli_query($con->con_db, $sqlCommand)){ 
+    echo "Your registration table has been created successfully!"; 
 } else { 
     echo " ERROR: registration table has not been created.";
 }
