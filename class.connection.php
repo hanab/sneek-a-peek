319817645442b920c56c01e4be2c511a19ab5ea7
Hana Demas <hb16826@gmail.com>
2016-02-08 14:01:34 +0000

    class.connection.php edited online with Bitbucket

--- class.connection.php
+++ class.connection.php
@@ -14,7 +14,7 @@ class Conncection {
     
     public function __construct(){
 8
-        $this->db = mysqli_connect($this->db_host,$this->db_user,$this->db_pass,$this->db_name);
+        $this->db = mysqli_connect($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
 9
             if(mysqli_connect_errno()) {
 11
