8c19a1c2a19be7af3809f86eab8d1fdc9be4dce7
Hana Demas <hb16826@gmail.com>
2016-02-08 17:19:52 +0100

    classes folder added

---
+++ myClasses/connection.php
@@ -0,0 +1,33 @@
+<?php
+namespace DatabaseHandler;
+
+
+class Conncection {
+
+    
+ private $host="127.0.0.1";
+ private $user="root";
+ private $pass="";
+ private $db="hanad";
+ public $con_db;
+     
+ 
+    
+    public function __construct(){
+
+        $this->con_db = mysqli_connect($this->host,$this->user,$this->pass,$this->db);
+
+            if(mysqli_connect_errno()) {
+
+                echo "Error: Could not connect to database.";
+                    exit;
+
+        }
+
+        }
+   }
+
+
+
+?>
+
