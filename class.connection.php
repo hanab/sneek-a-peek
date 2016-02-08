6897259c1edc2c925712b38e1644c66ed5c5948a
Hana Demas <hb16826@gmail.com>
2016-02-08 13:53:54 +0000

    class.connection.php edited online with Bitbucket

--- class.connection.php
+++ class.connection.php
@@ -4,16 +4,16 @@
 class Conncection {
 
     public $db;
-    protected $db_name = "hanad";
-    protected $db_user = "root";
-    protected $db_pass = "";
-    protected $db_host = "127.0.0.1";
+     $db_name = "hanad";
+     $db_user = "root";
+     $db_pass = "";
+     $db_host = "127.0.0.1";
  
     
     
     public function __construct(){
 8
-        $this->db = mysqli_connect($this->db_host,$this->db_user,$this->db_pass,$this->db_name);
+        $this->db = mysqli_connect(db_host,db_user,db_pass,db_name);
 9
             if(mysqli_connect_errno()) {
 11
