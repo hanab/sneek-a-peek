ff9388bb7b2caa6f6fa4e49112a674539368af5f
Hana Demas <hb16826@gmail.com>
2016-02-12 10:41:27 +0000

    register.php edited online with Bitbucket

--- myClasses/register.php
+++ myClasses/register.php
@@ -1,18 +1,18 @@
 <?php
  
  namespace DatabaseHandler;
-  //load using composer autoload
+  /** load using composer autoload */
   require_once __DIR__ . '/../vendor/autoload.php';
   use DatabaseHandler\connection;
-  
+  /** a class to register a user can be extended to add functionalities such as login and logout */
   class register {
-    //public fun to perform registration
+    /** public fun to perform registration */
      public function reg_user($name,$pass,$uemail) {
-            // define an obj of type connection
+            /** define an obj of type connection */
             $con = new connection();
-            //encrypte the password
+            /** encrypte the password */
             $password = md5($password);
-            //check if someone with this username or email already exists
+            /** check if someone with this username or email already exists */
             $sql="SELECT * FROM register WHERE username='$name' OR email='$uemail'";
             
             $check = mysqli_query($con->con_db,$sql);
@@ -20,7 +20,7 @@
             $count_row = $check->num_rows;
 
              if ($count_row == 0){
-                //if doesnt exist insert to the database
+                /** if doesnt exist insert to the database */
                 $sql1="INSERT INTO register SET username='$name', password='$pass', email='$uemail'";
 
                 $result = mysqli_query($con->con_db,$sql1) or die(mysqli_connect_errno()."Data cannot be inserted");
