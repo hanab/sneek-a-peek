2a619be860b3a556e74a292f779e803f30db671e
Hana Demas <hb16826@gmail.com>
2016-02-12 10:38:35 +0000

    connection.php edited online with Bitbucket

--- myClasses/connection.php
+++ myClasses/connection.php
@@ -1,19 +1,22 @@
 <?php
 namespace DatabaseHandler;
-
+/** a class to connect to the mysql database on the local machine */
 
 class connection {
 
-  //local host mysql databse variables  
+  /** the local host variable can be defined as constant as well */
  private $host="127.0.0.1";
+ /** the user name of the database */
  private $user="root";
+ /** the password for the database */
  private $pass="";
+ /** the name of the database where i want to create the table */
  private $db="hanad";
- //a connection variable to be accessed by other classes to connect to DB
+ /** a connection variable to be accessed by other classes to connect to DB */
  public $con_db;
-     
+      /** construct connection obj by seeting its public property */
    public function __construct(){
-        // construct connection obj by seeting its public property
+       
         $this->con_db = mysqli_connect($this->host,$this->user,$this->pass,$this->db);
 
             if(mysqli_connect_errno()) {
